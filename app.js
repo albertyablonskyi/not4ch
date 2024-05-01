491b2e7d411845f4e6b597a80e09f1dd9c4ffcdb
Albert Yablonskyi <albertyablonskyi@gmail.com>
2024-05-01 13:27:18 +0200

    Initial project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
